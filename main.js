37ded6937407802091a8cf2d66a24c149d61685b
何方 <he529564582@163.com>
2022-08-21 03:09:41 +0800

    add filter

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
+Vue.filter('formatCount',value =>{
+	if(value >= 10000 && value <= 100000000){
+		value  /= 10000
+		return value.toFixed(1) + "万"
+	}else if(value >100000000){
+		value /= 100000000
+		return value.toFixed(1) + "亿"
+	}else{
+		return value
+	}
+})
+
 App.mpType = 'app'
 const app = new Vue({
     ...App
