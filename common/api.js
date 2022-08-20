dc77f6445b1214f1e44aae7b09d541406709e24b
何方 <he529564582@163.com>
2022-08-21 03:10:25 +0800

    add list API

--- common/api.js
+++ common/api.js
@@ -1,7 +1,7 @@
 import {baseUrl} from './config.js'
 export function topList() {
-	
 	return new Promise((resolve, reject) => {
+		let listIds = ['19723756', '3779629', '2884035', '3778678']
 		uni.request({
 			url: `${baseUrl}/toplist/detail`,
 			method: 'GET',
@@ -9,8 +9,18 @@ export function topList() {
 			success: res => {
 				let result = res.data.list
 				result.length = 4
+				for (var i = 0; i < listIds.length; i++) {
+					result[i].listId = listIds[i]
+				}
 				resolve(result)
+				// console.log(res.data.list)
 			}
 		})
+	})
+}
+export function list(listId){
+	return uni.request({
+		url:`${baseUrl}/playlist/detail?id=${listId}`,
+		method:'GET'
 	})
 }
