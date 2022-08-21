ab0a67f89e834c88b4375f547acae091bbabf0f9
何方 <he529564582@163.com>
2022-08-22 00:46:58 +0800

    add 5个接口

--- common/api.js
+++ common/api.js
@@ -1,4 +1,8 @@
+
+
 import {baseUrl} from './config.js'
+// 获取所有榜单内容摘要
+// http://localhost:3000/toplist/detail
 export function topList() {
 	return new Promise((resolve, reject) => {
 		let listIds = ['19723756', '3779629', '2884035', '3778678']
@@ -18,9 +22,56 @@ export function topList() {
 		})
 	})
 }
+// 获取歌单详情动态
+// http://localhost:3000/playlist/detail?id=19723756
 export function list(listId){
 	return uni.request({
 		url:`${baseUrl}/playlist/detail?id=${listId}`,
+		method:'GET'
+	})
+}
+
+//  获取所有歌曲的详情
+// http://localhost:3000/song/detail?ids=19723756
+export function songDetail(id){
+	return uni.request({
+		url:`${baseUrl}/song/detail?ids=${id}`,
+		method:'GET'
+	})
+}
+
+// 获得相似歌曲
+// http://localhost:3000/simi/song?id=19723756
+export function songSimi(id){
+	return uni.request({
+		url:`${baseUrl}/simi/song?id=${id}`,
+		method:'GET'
+	})
+}
+
+// 获取该音乐的所有评论 
+// http://localhost:3000/comment/music?id=19723756
+export function songComment(id){
+	return uni.request({
+		url:`${baseUrl}/comment/music?id=${id}`,
+		method:'GET'
+	})
+}
+
+// 获取歌词
+// http://localhost:3000/lyric?id=19723756
+export function songLyric(id){
+	return uni.request({
+		url:`${baseUrl}/lyric?id=${id}`,
+		method:'GET'
+	})
+}
+
+// 获取的是歌曲试听url
+// http://localhost:3000/song/url?id=19723756
+export function songUrl(id){
+	return uni.request({
+		url:`${baseUrl}/song/url?id=${id}`,
 		method:'GET'
 	})
 }
